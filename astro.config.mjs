129f1aae424ffeed2531a9ff20b1dde9e2b98013
gurprit-singh <75479921+gurprit-singh@users.noreply.github.com>
2025-07-13 21:46:01 +0530

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "http://ai.triasiaglobal.com",
+  site: "https://ai.triasiaglobal.com",
   base: "/",
   integrations: [tailwind()],
   output: 'static',
